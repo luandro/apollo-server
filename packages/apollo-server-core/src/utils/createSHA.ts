359c8bfd0efbe60c4f75645fb8d64f860ddc1401
luandro <luandro@gmail.com>
2020-01-22 16:20:34 -0300

    require instead of module.require

--- packages/apollo-server-core/src/utils/createSHA.ts
+++ packages/apollo-server-core/src/utils/createSHA.ts
@@ -4,7 +4,7 @@ export default function(kind: string): import('crypto').Hash {
   if (isNodeLike) {
     // Use module.require instead of just require to avoid bundling whatever
     // crypto polyfills a non-Node bundler might fall back to.
-    return module.require('crypto').createHash(kind);
+    return require('crypto').createHash(kind);
   }
   return require('sha.js')(kind);
 }
